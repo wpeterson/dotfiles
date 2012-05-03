68612fc7e422356869a964789e266dd5ebc8f65c
Winfield Peterson <wpeterson@brightcove.com>
2012-05-03 14:39:30 -0400

    Add initial dotfiles.

---
+++ .bash_profile
@@ -0,0 +1,44 @@
+source ~/.ec2/.aws_keys
+
+if [ -f `brew --prefix`/etc/bash_completion ]; then
+  . `brew --prefix`/etc/bash_completion
+fi
+
+export PATH=/Applications/GitX.app/Contents/Resources:$PATH
+export PATH=~/scripts:$PATH
+export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+export PATH=./.bin:$PATH
+
+source /Users/local/VIDMARK/wpeterson/.rvm/scripts/rvm
+
+export CLICOLOR=1
+export LSCOLORS=ExFxCxDxBxegedabagacad
+
+export PS1='\[\e[1;34m\][$(__git_ps1 "%s") \[\e[0m\]\w\[\e[1;34m\]]\$> \[\e[0m\]'
+export PS1="\[\033[G\]$PS1"
+
+# Readline Config
+bind 'set completion-ignore-case on'
+bind 'set completion-prefix-display-length 2'
+bind 'set show-all-if-ambiguous on'
+
+alias gpo="git push origin \$(__git_ps1 %s)"
+alias gpoh='git push origin head'
+alias gg='git grep -in'
+alias ggc='git grep -in --function-context'
+alias gpr='git pull --rebase --stat'
+alias git-scoreboard="git shortlog -n -s -e"
+alias master='git checkout master'
+
+alias b='bundle exec'
+alias ks='cd /work/katama_server'
+alias kit='cd /work/katama_image_transcoder'
+alias kr='cd /work/katama_runtime'
+alias mmd='cd /work/mock_maitred_server'
+
+alias dev_ssh='~/scripts/katama_ssh ~/.ec2/katama-integration.pem'
+alias prod_ssh='~/scripts/katama_ssh ~/.ec2/katama-integration.pem'
+
+ks
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
---
+++ .chef/knife.rb
@@ -0,0 +1,46 @@
+current_dir = File.dirname(__FILE__)
+log_level                 :info
+log_location              STDOUT
+node_name                 "wpeterson"
+client_key                "#{current_dir}/wpeterson.pem"
+validation_client_name    "brightcove-appcloud-validator"
+validation_key            "#{current_dir}/brightcove-appcloud-validator.pem"
+chef_server_url           "https://api.opscode.com/organizations/brightcove-appcloud"
+cache_type                 'BasicFile'
+cache_options( :path =>   "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path             ["#{current_dir}/../cookbooks"]
+cookbook_copyright        "Brightcove, Inc."
+cookbook_email            "wpeterson@brightcove.com"
+
+## default values
+knife[:flavor] = 'm1.large'
+knife[:image] = 'ami-4fa37426' # oneiric ocelot us-east-1 64-bit instance-store
+encrypted_data_bag_secret "#{current_dir}/../data_bags/data_bag_key"
+
+env = ENV['CHEF_ENV']
+if env == "production"
+  puts "INFO: using production environment"
+  environment "production"
+  knife[:aws_access_key_id] = ENV['AWS_PROD_ACCESS_KEY']
+  knife[:aws_secret_access_key] = ENV['AWS_PROD_SECRET_KEY']
+  knife[:availability_zone] = 'us-east-1c'
+  knife[:aws_ssh_key_id] = 'katama-prod'
+  #knife[:identity_file] = "/Users/wpeterson/.ec2/katama-prod.pem"
+
+elsif env == "staging"
+  puts "INFO: using staging environment"
+  environment "staging"
+  knife[:aws_access_key_id] = ENV['AWS_QA_ACCESS_KEY']
+  knife[:aws_secret_access_key] = ENV['AWS_QA_SECRET_KEY']
+  knife[:availability_zone] = 'us-west-1c'
+  knife[:aws_ssh_key_id] = 'katama_integration'
+  #knife[:identity_file] = "/Users/wpeterson/.ec2/katama_integration.pem"
+
+else # must be QA
+  environment "qa"
+  knife[:aws_access_key_id] = ENV['AWS_QA_ACCESS_KEY']
+  knife[:aws_secret_access_key] = ENV['AWS_QA_SECRET_KEY']
+  knife[:availability_zone] = 'us-east-1c'
+  knife[:aws_ssh_key_id] = 'katama_integration'
+  #knife[:identity_file] = "/Users/wpeterson/.ec2/katama_integration.pem"
+end
