4718ae9c0a566fe97394cdfdd121d7568863fea7
Winfield Peterson <wpeterson@brightcove.com>
2012-07-25 15:27:50 -0400

    Update knife config for transition to central chef from hosted chef.

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -2,15 +2,12 @@ current_dir = File.dirname(__FILE__)
 log_level                 :info
 log_location              STDOUT
 node_name                 "wpeterson"
-client_key                "#{current_dir}/wpeterson.pem"
-validation_client_name    "brightcove-appcloud-validator"
-validation_key            "#{current_dir}/brightcove-appcloud-validator.pem"
-chef_server_url           "https://api.opscode.com/organizations/brightcove-appcloud"
 cache_type                 'BasicFile'
 cache_options( :path =>   "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path             ["/work/cookbooks/cookbooks"]
 cookbook_copyright        "Brightcove, Inc."
 cookbook_email            "wpeterson@brightcove.com"
+
 
 ## default values
 knife[:flavor] = 'm1.large'
@@ -19,28 +16,30 @@ encrypted_data_bag_secret "/work/cookbooks/data_bags/data_bag_key"
 
 env = ENV['CHEF_ENV']
 if env == "production"
+  # Hosted Chef
+  chef_server_url           "https://api.opscode.com/organizations/brightcove-appcloud"
+  validation_client_name    "brightcove-appcloud-validator"
+  validation_key            "#{current_dir}/brightcove-appcloud-validator.pem"
+  client_key                "#{current_dir}/hosted-wpeterson.pem"
+
   puts "INFO: using production environment"
   environment "production"
   knife[:aws_access_key_id] = ENV['AWS_PROD_ACCESS_KEY']
   knife[:aws_secret_access_key] = ENV['AWS_PROD_SECRET_KEY']
 #  knife[:availability_zone] = 'us-east-1c'
   knife[:aws_ssh_key_id] = 'katama-prod'
-  #knife[:identity_file] = "/Users/wpeterson/.ec2/katama-prod.pem"
-
-elsif env == "staging"
-  puts "INFO: using staging environment"
-  environment "staging"
-  knife[:aws_access_key_id] = ENV['AWS_QA_ACCESS_KEY']
-  knife[:aws_secret_access_key] = ENV['AWS_QA_SECRET_KEY']
-#  knife[:availability_zone] = 'us-west-1c'
-  knife[:aws_ssh_key_id] = 'katama_integration'
-  #knife[:identity_file] = "/Users/wpeterson/.ec2/katama_integration.pem"
-
+  knife[:identity_file] = "/Users/wpeterson/.ec2/katama-prod.pem"
 else # must be QA
+  # Brightcove chef
+  chef_server_url           "http://chef.ops.brightcove.com:4000"
+  validation_client_name    "chef-validator"
+  validation_key            "#{current_dir}/central-chef-validator.pem"
+  client_key                "#{current_dir}/syseng-wpeterson.pem"
+
   environment "qa"
   knife[:aws_access_key_id] = ENV['AWS_QA_ACCESS_KEY']
   knife[:aws_secret_access_key] = ENV['AWS_QA_SECRET_KEY']
 #  knife[:availability_zone] = 'us-east-1c'
   knife[:aws_ssh_key_id] = 'katama_integration'
-  #knife[:identity_file] = "/Users/wpeterson/.ec2/katama_integration.pem"
+  knife[:identity_file] = "/Users/wpeterson/.ec2/katama-integration.pem"
 end
