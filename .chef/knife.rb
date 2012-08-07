c806d0bbb203df78633a293bf5a3f39f79024ec2
Winfield Peterson <wpeterson@brightcove.com>
2012-08-07 13:54:01 -0400

    Switchover to SysEng chef server.

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -14,14 +14,20 @@ knife[:flavor] = 'm1.large'
 knife[:image] = 'ami-4fa37426' # oneiric ocelot us-east-1 64-bit instance-store
 encrypted_data_bag_secret "/work/cookbooks/data_bags/data_bag_key"
 
-env = ENV['CHEF_ENV']
-if env == "production"
-  # Hosted Chef
-  chef_server_url           "https://api.opscode.com/organizations/brightcove-appcloud"
-  validation_client_name    "brightcove-appcloud-validator"
-  validation_key            "#{current_dir}/brightcove-appcloud-validator.pem"
-  client_key                "#{current_dir}/hosted-wpeterson.pem"
+# Hosted chef
+# chef_server_url           "https://api.opscode.com/organizations/brightcove-appcloud"
+# validation_client_name    "brightcove-appcloud-validator"
+# validation_key            "#{current_dir}/brightcove-appcloud-validator.pem"
+# client_key                "#{current_dir}/hosted-wpeterson.pem"
 
+# Brightcove chef
+chef_server_url           "http://chef.ops.brightcove.com:4000"
+validation_client_name    "chef-validator"
+validation_key            "#{current_dir}/central-chef-validator.pem"
+client_key                "#{current_dir}/syseng-wpeterson.pem"
+
+env = ENV['CHEF_ENV']
+if env == "production" || env == 'katama_production'
   puts "INFO: using production environment"
   environment "production"
   knife[:aws_access_key_id] = ENV['AWS_PROD_ACCESS_KEY']
@@ -30,12 +36,6 @@ if env == "production"
   knife[:aws_ssh_key_id] = 'katama-prod'
   knife[:identity_file] = "/Users/wpeterson/.ec2/katama-prod.pem"
 else # must be QA
-  # Brightcove chef
-  chef_server_url           "http://chef.ops.brightcove.com:4000"
-  validation_client_name    "chef-validator"
-  validation_key            "#{current_dir}/central-chef-validator.pem"
-  client_key                "#{current_dir}/syseng-wpeterson.pem"
-
   environment "qa"
   knife[:aws_access_key_id] = ENV['AWS_QA_ACCESS_KEY']
   knife[:aws_secret_access_key] = ENV['AWS_QA_SECRET_KEY']
