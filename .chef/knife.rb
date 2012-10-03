ae9f39431795611e38499671fc1b52e2d5292918
Winfield Peterson <wpeterson@brightcove.com>
2012-10-03 12:09:46 -0400

    Cleanup paths to use home directory.

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -16,17 +16,11 @@ knife[:image] = 'ami-4fa37426' # oneiric ocelot us-east-1 64-bit instance-store
 knife[:ssh_attribute] = 'name'
 encrypted_data_bag_secret "/work/chef/data_bags/data_bag_key"
 
-# Hosted chef
-# chef_server_url           "https://api.opscode.com/organizations/brightcove-appcloud"
-# validation_client_name    "brightcove-appcloud-validator"
-# validation_key            "#{current_dir}/brightcove-appcloud-validator.pem"
-# client_key                "#{current_dir}/hosted-wpeterson.pem"
-
 # Brightcove chef
 chef_server_url           "https://chef.ops.brightcove.com:4443"
 validation_client_name    "chef-validator"
-validation_key            "#{current_dir}/central-chef-validator.pem"
-client_key                "#{current_dir}/syseng-wpeterson.pem"
+validation_key            "~/.chef/central-chef-validator.pem"
+client_key                "~/.chef/syseng-wpeterson.pem"
 
 env = ENV['CHEF_ENV']
 if env == "production" || env == 'katama_production'
@@ -36,12 +30,12 @@ if env == "production" || env == 'katama_production'
   knife[:aws_secret_access_key] = ENV['AWS_PROD_SECRET_KEY']
 #  knife[:availability_zone] = 'us-east-1c'
   knife[:aws_ssh_key_id] = 'katama-prod'
-  knife[:identity_file] = "/Users/wpeterson/.ec2/katama-prod.pem"
+  knife[:identity_file] = "~/.ec2/katama-prod.pem"
 else # must be QA
   environment "qa"
   knife[:aws_access_key_id] = ENV['AWS_QA_ACCESS_KEY']
   knife[:aws_secret_access_key] = ENV['AWS_QA_SECRET_KEY']
 #  knife[:availability_zone] = 'us-east-1c'
   knife[:aws_ssh_key_id] = 'katama_integration'
-  knife[:identity_file] = "/Users/wpeterson/.ec2/katama-integration.pem"
+  knife[:identity_file] = "~/.ec2/katama-integration.pem"
 end
